58aaac99919987939671cb47d0a3a512fa371a9b
atiladefreitas <atila.defreitas@icloud.com>
2023-03-16 19:43:48 -0300

    Added AOS, transfered first section to external component

---
+++ src/pages/Products/Sections/FirstSection.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Container } from "./styles";
+
+interface IFirstSectionProp {
+  call: string;
+  description: string;
+  photoBase64: string;
+  src: any;
+  backgroundColor: any;
+  onClick: any;
+}
+
+function FirstSection({backgroundColor, call, description, photoBase64, src, onClick}: IFirstSectionProp): JSX.Element {
+  return (
+    <Container style={{backgroundColor: backgroundColor}}>
+        <div
+          className={'first-wrapper'}
+        >
+          <h1>{call}</h1>
+          <p>{description}</p>
+          <div className="img-wrapper"  data-aos="fade-up">
+            {photoBase64 == '' ? (
+              <img fetch-priority={'hight'} src={src}  alt="Foto de capa exemplo" loading='lazy'/>
+            ) : (
+              <img fetch-priority={'hight'} src={photoBase64} alt={'foto de capa'} loading='lazy'/>
+            )}
+          </div>
+          <button onClick={onClick}>Vamos conversar!</button>
+        </div>
+    </Container>
+  )
+}
+
+export { FirstSection }
---
+++ src/pages/Products/Sections/styles.ts
@@ -0,0 +1,84 @@
+import styled from "styled-components";
+
+//Banner
+export const Container = styled.div`
+  width: 100%;
+  height: fit-content;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgb(5, 55, 124);
+
+  & .first-wrapper {
+    width: 50%;
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 4rem 2rem;
+    gap: 2rem;
+
+    @media screen and (max-width: 800px) {
+      width: 100%;
+      padding: 10% 5%;
+    }
+
+    & h1 {
+      margin: 0;
+      color: white;
+      line-height: 40px;
+      text-align: center;
+      padding: 0 1.5rem 0;
+      font-family: "Montserrat", sans-serif;
+
+      @media screen and (max-width: 410px) {
+        font-size: 6vw;
+      }
+      @media screen and (max-width: 330px) {
+        font-size: 18px;
+      }
+    }
+
+    & p {
+      margin: 0;
+      font-weight: 500;
+      font-size: 17px;
+      color: #f0f0f0;
+      line-height: 33px;
+      font-family: "Montserrat", sans-serif;
+
+      text-align: center;
+    }
+
+    & .img-wrapper {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 0;
+      padding: 0;
+      width: fit-content;
+      max-width: 100%;
+      max-height: 70vh;
+
+      @media screen and (max-width: 800px) {
+        margin: 0;
+        padding: 0;
+        width: fit-content;
+        max-width: 100%;
+        max-height: 60vh;
+      }
+    }
+
+    & img {
+      max-width: 90%;
+      max-height: 70vh;
+      margin-bottom: 0.5rem;
+      border-radius: 1rem;
+      box-shadow: 0px 5px 5px 0 rgba(68, 67, 67, 0.2);
+
+      @media screen and (max-width: 800px) {
+        max-height: 80vh;
+      }
+    }
+  }
+`;
