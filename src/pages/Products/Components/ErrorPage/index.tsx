250233a2ff7f0b6bc537f377f5c6e0f2a45cf33f
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-03-30 15:58:20 -0300

    Página de erro

---
+++ src/pages/Products/Components/ErrorPage/index.tsx
@@ -0,0 +1,49 @@
+import { Container } from './styles';
+import Typewriter from 'typewriter-effect';
+
+const typewriter404 = () => {
+  return(
+    <Container>
+      <div className='typewriterWrapper' style={{backgroundColor:'black'}}>
+
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter.typeString('404')
+              .pauseFor(300)
+              .callFunction(() => {
+                const cursor = document.querySelector('.Typewriter__cursor') as HTMLElement;
+                if (cursor) {
+                  cursor.style.display = 'none';
+                }
+              })
+              .start();
+          }}
+        />
+
+        <Typewriter
+          onInit={(typewriter) => {
+            setTimeout(() => {
+              typewriter.typeString('Página não encontrada!')
+                .pauseFor(300)
+                .callFunction( function() {
+                  const cursor = document.querySelector('.Typewriter__cursor') as HTMLElement;
+                  if (cursor) {
+                    cursor.style.display = 'none';
+                  }
+                })
+                .start();
+            }, 1500);
+          }}
+        />
+
+        <h3>Confira se o link acessado está correto!</h3>
+
+        <button>Suporte</button>
+
+      </div>
+    </Container>
+  );
+};
+
+export default typewriter404;
+
