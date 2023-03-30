f4877626746311e545b7bcd8c1f3fbfee35f36c1
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-03-30 16:22:00 -0300

    correção altura do texto

--- src/pages/Products/Components/ErrorPage/styles.tsx
+++ src/pages/Products/Components/ErrorPage/styles.tsx
@@ -27,7 +27,7 @@ export const Container = styled.section`
     @media screen and (max-width: 800px){
       font-size: 3rem;
       text-align: center;
-      min-height: 20vh;
+      min-height: 15vh;
     }
   }
 
