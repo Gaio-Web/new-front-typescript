0132b23b28b795b771d5d77636fe4b07ff0c27f8
Matheus Rodela <46487106+matheusrodela@users.noreply.github.com>
2023-04-03 18:54:42 -0300

    api funcionando

--- src/pages/Products/Components/UnsplashAPI/UnsplashAPI.tsx
+++ src/pages/Products/Components/UnsplashAPI/UnsplashAPI.tsx
@@ -1,10 +1,51 @@
 /* eslint-disable linebreak-style */
 import { Container } from './styles';
-import React from 'react';
+import React,{ createContext } from 'react';
 import useAxios from '../../../../hooks/useAxios';
+import ReactLoading from 'react-loading';
 
-function unsplashImages() {
-  const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=office&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
+import Image from './Image';
+interface ImageData {
+  urls: {
+    small: string;
+  };
+  alt_description: string;
 }
 
-export default unsplashImages;
+interface ImageContextData {
+  response: any;
+  isLoading: boolean;
+  error: any;
+  fetchData: (url: string) => Promise<void>;
+  }
+
+
+export const ImageContext = createContext<ImageContextData>({
+  response: null,
+  isLoading: true,
+  error: null,
+  fetchData: () => Promise.resolve()
+});
+
+function UnsplashCoverImage ({ data }: { data: ImageData }) {
+  // const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=office&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
+  const { response, isLoading, error, fetchData } = useAxios('search/photos?page=1&orientation=portrait&query=Makeup+beauty+store&client_id=gz6269fjUdh2U2Ne7qlX0jBilMNMnCPSGToMMVGuH0o');
+
+
+  const value: ImageContextData = {
+    response,
+    isLoading,
+    error,
+    fetchData
+  };
+
+  return(
+    <ImageContext.Provider value={value}>
+      <Container>
+        {isLoading ? <ReactLoading type={'spin'} color={'#eee'} height={200} width={100}/> : response.length > 0 && <Image data={response[0]} />}
+      </Container>
+    </ImageContext.Provider>
+  );
+}
+
+export default UnsplashCoverImage;
