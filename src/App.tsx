e0cb8ce75d27cc34c7257ed25bb40400868eb1a4
Átila de Freitas <atila.defreitas@icloud.com>
2023-07-01 10:00:39 -0300

    removing old form import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import FindByPhone from './pages/Products/Products';
-import { Form } from './pages/Form/Form';
 import { BrowserRouter } from 'react-router-dom';
 import AnimatedRoutes from './Routes/animatedRoutes';
 
